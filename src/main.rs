cf6be20145b17a4e0ec2eb79503c5ace7531811d
objz <me@objz.dev>
2025-06-20 21:34:54 +0200

    base iced app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-    println!("Hello, world!");
+use iced::widget::text;
+use iced::{Application, Command, Element, Settings};
+
+pub struct Papyrust {}
+
+#[derive(Debug, Clone)]
+pub enum Message {}
+
+impl Application for Papyrust {
+    type Message = Message;
+
+    type Flags = ();
+
+    type Theme = iced::theme::Theme;
+
+    type Executor = iced::executor::Default;
+
+    fn new(_flags: Self::Flags) -> (Self, Command<Self::Message>) {
+        (Papyrust {}, Command::none())
+    }
+
+    fn title(&self) -> String {
+        String::from("Papyrust")
+    }
+
+    fn theme(&self) -> Self::Theme {
+        iced::theme::Theme::GruvboxDark
+    }
+
+    fn update(&mut self, _message: Self::Message) -> Command<Self::Message> {
+        Command::none()
+    }
+
+    fn view(&self) -> Element<Self::Message> {
+        text("Papyrust").into()
+    }
+}
+
+fn main() -> iced::Result {
+    Papyrust::run(Settings::default())
 }
